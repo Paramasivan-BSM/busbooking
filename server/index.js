cb1248a742522f667d2fb0a97da381bd31a72948
paramasivan <paramasivan645@gmail.com>
2025-04-07 12:31:04 +0530

    .

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 app.use(
     cors({
-      origin: "http://localhost:5173", // No trailing slash!
+      origin: "http://localhost:5173/", // No trailing slash!
       credentials: true,
     })
   );
